af24d7bf91e08ed0ad8ac28aaa2b0b89e4dae7bf
Martin Günther <martin.guenther@dfki.de>
2024-06-13 17:52:34 +0200

    Add fake_anchoring_node

---
+++ daa_color_classification/nodes/fake_anchoring_node.py
@@ -0,0 +1,117 @@
+#!/usr/bin/env python
+
+# Copyright (c) 2023, DFKI GmbH and contributors
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+#    * Redistributions of source code must retain the above copyright
+#      notice, this list of conditions and the following disclaimer.
+#
+#    * Redistributions in binary form must reproduce the above copyright
+#      notice, this list of conditions and the following disclaimer in the
+#      documentation and/or other materials provided with the distribution.
+#
+#    * Neither the name of the the copyright holder nor the names of its
+#      contributors may be used to endorse or promote products derived from
+#      this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+# LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+# SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+# CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+
+import message_filters
+import rospy
+from daa_color_classification.msg import ColorClassificationResults, Colors
+from object_pose_msgs.msg import ObjectList, ObjectPose
+from vision_msgs.msg import Detection3DArray, Detection3D
+
+COLORS_TO_INSTANCE_IDS = {
+    "Traffic Red": 1,  # red_part
+    "Traffic Yellow": 2,  # yellow_part
+    "Yellow Green": 3,  # bright_green_part
+    "Traffic Green": 4,  # dark_green_part
+    "Purple": 5,  # purple_part
+    "Telemagenta": 6,  # magenta_part
+}
+
+
+class FakeAnchoringNode:
+    def __init__(self):
+        # parameters
+        self._class_ids = rospy.get_param('~class_ids')
+
+        # publishers
+        self._pub_object_list = rospy.Publisher('anchored_objects', ObjectList, queue_size=10)
+
+        # subscribers
+        detected_objs_sub = message_filters.Subscriber('dope/detected_objects', Detection3DArray)
+        color_classification_sub = message_filters.Subscriber('color_classification', ColorClassificationResults)
+        ts = message_filters.TimeSynchronizer([detected_objs_sub, color_classification_sub], queue_size=100)
+        ts.registerCallback(self._msg_callback)
+
+    def _msg_callback(self, detection_array: Detection3DArray, colors: ColorClassificationResults):
+        output_msg = ObjectList()
+        output_msg.header = detection_array.header
+        for det, colors in zip(detection_array.detections, colors.colors):
+            if len(det.results) != 1:
+                rospy.logerr('Expected exactly 1 object hypothesis per detection, got %d', len(det.results))
+                continue
+            if det.results[0].id != self._class_ids['klt']:
+                continue
+
+            output_msg.objects.append(self._make_output_pose(det, colors))
+
+        self._pub_object_list.publish(output_msg)
+
+    def _make_output_pose(self, det: Detection3D, colors: Colors):
+        output_pose = ObjectPose()
+
+        object_hypothesis = det.results[0]
+        output_pose.pose = object_hypothesis.pose.pose
+        output_pose.size = det.bbox.size
+        # output_pose.min and output_pose.max left empty (not used)
+        class_id_to_name = {class_id: name for name, class_id in self._class_ids.items()}
+        output_pose.class_id = class_id_to_name[object_hypothesis.id]
+
+        # find maximum color
+        max_color = None
+        max_intensity = float('-inf')
+        for color, intensity in zip(colors.colors, colors.intensities):
+            if intensity == 0.0:
+                continue
+            if color not in COLORS_TO_INSTANCE_IDS:  # should only be "Blue"
+                continue
+            if max_intensity < intensity:
+                max_intensity = intensity
+                max_color = color
+
+        # "fake anchoring": assign instance ID based on maximum color of contents
+        if max_color is None:
+            output_pose.instance_id = 0
+        else:
+            output_pose.instance_id = COLORS_TO_INSTANCE_IDS[max_color]
+
+        return output_pose
+
+
+def main():
+    rospy.init_node('color_object_publisher')
+    FakeAnchoringNode()
+
+    try:
+        rospy.spin()
+    except rospy.ROSInterruptException:
+        pass
+
+
+if __name__ == '__main__':
+    main()
